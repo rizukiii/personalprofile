f9652ccc914fa2cb3cbee767c7e29d354a158fe0
rizukiii <182581582+rizukiii@users.noreply.github.com>
2024-09-26 10:21:44 +0700

    fixing head auth

--- backend/pages/account/login.php
+++ backend/pages/account/login.php
@@ -1,34 +1,12 @@
-<?php
-include "../../app.php";
-session_start();
-
-$query = "SELECT COUNT(*) as total FROM `tb_account`";
-$result = $connect->query($query);
-$row = $result->fetch_object();
-
-?>
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-<link rel="icon" href="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ5e9cvq1MwhKvDqgdVOTS7PsWlCeMt3QmFZg&s">
-</head>
+<?php include "../../components/head_auth.php" ?>
 
 <body style="background-image: url(https://wallpapers.com/images/hd/coding-background-9izlympnd0ovmpli.jpg); object-fit: cover; object-position: center;">
-    <div class="container-fluid " >
+    <div class="container-fluid ">
         <div class="row justify-content-center align-items-center d-flex" style=" height: 100vh;">
             <div class="col-6 ">
                 <div class="card shadow">
                     <div class="card-header d-flex">
                         <h3 class="card-title">Login</h3>
-                        <?php
-                        if ($row->total < 1) {
-                            echo '<a href="register.php" class="btn btn-primary ms-auto">Register</a>';
-                        } ?>
                     </div>
                     <div class="card-body">
                         <form action="../../actions/account/login.php" method="post">
--- backend/pages/account/register.php
+++ backend/pages/account/register.php
@@ -1,25 +1,4 @@
-<?php
-include "../../app.php";
-session_start();
-
-$query = mysqli_query($connect, "SELECT COUNT(*) as total FROM `tb_account`");
-$result = $query->fetch_object();
-if ($result->total >= 1) {
-    echo "<script>window.location.href='../../pages/account/login.php'</script>";
-} ?>
-
-
-
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Register</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <link rel="icon" href="https://cdn-icons-png.freepik.com/256/15181/15181819.png?semt=ais_hybrid">
-</head>
+<?php include "../../components/head_auth.php" ?>
 
 <body style="background-image: url(https://wallpapers.com/images/hd/coding-background-9izlympnd0ovmpli.jpg); object-fit: cover; object-position: center;">
     <div class="container-fluid">
